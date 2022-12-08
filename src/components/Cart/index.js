a2ea7c1e98d045909a5bbede54ad65e53d026bec
Your Name <you@example.com>
2022-12-08 15:30:36 +0000

    export and import

---
+++ src/components/Cart/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Cart = () => (
+  <div className="products-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+      className="image"
+    />
+  </div>
+)
+
+export default Cart
